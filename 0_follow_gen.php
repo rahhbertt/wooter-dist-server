99f5977bd0909aec1dd16a066e3882f6b1fa5d02
rahhbertt <rahhbertt@users.noreply.github.com>
2015-04-15 23:15:32 -0400

    create_new_user(), read_user(), read_id(), and follow() all seem to still work. unfollow needs more testing, occasional race condition.. or network related problem somehow? test on c++ end to see if ever get same problem

--- 0_follow_gen.php
+++ 0_follow_gen.php
@@ -4,7 +4,7 @@ $user_obj = new User;
 // make 20 users, so that at least 20 exist if none so far
 
 if(!file_exists("fids/fids.txt")){
-	for($usernum=0; $usernum<20; $usernum++){
+	for($usernum=0; $usernum<21; $usernum++){
 		$user_obj->username="test_acc".$usernum;
 		$user_obj->password="test_psswd".$usernum;
 		$user_obj->full_name="Test's_First_name Test's_Last_name_+_".$usernum;
@@ -24,7 +24,7 @@ else {
 			$other_obj=read_user("test_acc".(($usernum+$offset+$offset2))%20); 
 			echo "<br><br>You are trying to get $user_obj->username to follow $other_obj->username.<br>"; 
 			if(!empty($other_obj) && $user_obj->id!=$other_obj->id){
-				follow($user_obj, $other_obj); 
+				unfollow($user_obj, $other_obj); 
 			}
 		}
 	}
--- 1_follow_gen.php
+++ 1_follow_gen.php
@@ -4,7 +4,7 @@ $user_obj = new User;
 // make 20 users, so that at least 20 exist if none so far
 
 if(!file_exists("fids/fids.txt")){
-	for($usernum=40; $usernum>=20; $usernum--){
+	for($usernum=20; $usernum>=0; $usernum--){
 		$user_obj->username="test_acc".$usernum;
 		$user_obj->password="test_psswd".$usernum;
 		$user_obj->full_name="Test's_First_name Test's_Last_name_+_".$usernum;
@@ -13,17 +13,19 @@ if(!file_exists("fids/fids.txt")){
 		create_new_user($user_obj);
 	}
 }
-$usernum=0; $offset=1;
-for($usernum=40; $usernum>=20; $usernum--){
-	for($offset=10; $offset<35; $offset++){
-		echo "User num: ".$usernum." Offset: ".($offset%20)."<br>";
-		$user_obj=read_user("test_acc".$usernum);
-		if( ($usernum+$offset)%20 == $offset%20) { $offset2=1;} 
-		else { $offset2=0; } 
-		$other_obj=read_user("test_acc".(($usernum+$offset+$offset2))%20); 
-		echo "<br><br>You are trying to get $user_obj->username to follow $other_obj->username.<br>"; 
-		if(!empty($other_obj) && $user_obj->id!=$other_obj->id){
-			unfollow($user_obj, $other_obj); 
+else {
+	$usernum=0; $offset=1;
+	for($usernum=20; $usernum>=0; $usernum--){
+		for($offset=10; $offset<35; $offset++){
+			echo "User num: ".$usernum." Offset: ".($offset%20)."<br>";
+			$user_obj=read_user("test_acc".$usernum);
+			if( ($usernum+$offset)%20 == $offset%20) { $offset2=1;} 
+			else { $offset2=0; } 
+			$other_obj=read_user("test_acc".(($usernum+$offset+$offset2))%20); 
+			echo "<br><br>You are trying to get $user_obj->username to follow $other_obj->username.<br>"; 
+			if(!empty($other_obj) && $user_obj->id!=$other_obj->id){
+				follow($user_obj, $other_obj); 
+			}
 		}
 	}
 }
